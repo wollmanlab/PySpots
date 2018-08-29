2a148ab10ad38c92ab887090842303b6ca54eb25
rfor10 <rfor10@gmail.com>
2018-08-28 18:44:58 -0700

    made paths relative

--- hybescope_config/microscope_config.py
+++ hybescope_config/microscope_config.py
@@ -6,11 +6,10 @@ from skimage.io import imread
 ################################ Microscope Related Config ##########################
 image_size = (2048, 2048)
 
-base_pth = '/home/rfor10/repos/PySpots'
+base_pth = os.path.dirname(__file__) # finds location of this module
 ave_bead = pickle.load(open(os.path.join(base_pth, 'ave_bead.333um.pkl'), 'rb'))
 
-# This .pkl file needs to be in the spot_calling repository/directory
-chromatic_dict = pickle.load(open(os.path.join(base_pth, './jan2018_chromatic.pkl'), 'rb')) # Warning File import
+chromatic_dict = pickle.load(open(os.path.join(base_pth, 'jan2018_chromatic.pkl'), 'rb')) # Warning File import
 
 xshift_fr = numpy.add(chromatic_dict['orange_minus_farred'][0], range(image_size[0]))
 yshift_fr = numpy.add(chromatic_dict['orange_minus_farred'][1], range(image_size[1]))
@@ -27,6 +26,6 @@ farred_psf = farred_psf/farred_psf.sum()
 green_psf = imread(os.path.join(base_pth, 'green_psf_fit_250nmZ_63x.tif'))
 green_psf = green_psf[28, 5:14, 5:14]
 green_psf = green_psf/green_psf.sum()
-orange_psf = imread(os.path.join(base_pth, '63x_psf_orange_250nmZ.tif'))
+orange_psf = imread(os.path.join(base_pth, 'orange_psf_fit_250nmZ_63x.tif'))
 orange_psf = orange_psf[25,  5:14, 5:14]
 orange_psf = orange_psf/orange_psf.sum()
