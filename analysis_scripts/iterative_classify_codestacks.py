8bfe56f4cf3adfd9edc98f70389c5cf7587ea1a7
rfor10 <rfor10@gmail.com>
2018-09-05 17:55:56 -0700

    added python program to do iterative analysis

---
+++ analysis_scripts/iterative_classify_codestacks.py
@@ -0,0 +1,220 @@
+from skimage import io
+import os
+import numpy
+import numpy as np
+#from hybescope_config.microscope_config import *
+#from metadata import Metadata
+from functools import partial
+import importlib
+import multiprocessing
+import pickle
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cstk_path", type=str, help="Path to folder containing codestack npz files.")
+    parser.add_argument("cword_config", type=str, help="Path to python file initializing the codewords and providing bitmap variable.")
+#     parser.add_argument("--posnames", type=str, help="Path pickle dictionary of tforms per position name.")
+    #parser.add_argument("out_path", type=str, help="Path to save output.")
+    parser.add_argument("-p", "--nthreads", type=int, dest="ncpu", default=8, action='store', help="Number of cores to utilize (default 12).")
+    parser.add_argument("--posnames", dest="posnames", nargs='*', type=str, default=[], action='store', help="Number z-slices above and below to max project together.")
+    parser.add_argument("-n", "--niter", type=int, dest="niter", default=10, action='store', help="Number of iterations to perform.")
+#     parser.add_argument("-m", "--zmax", type=int, dest="zmax", default=15, action='store', help="End making max projections centered at zmax.")
+#     parser.add_argument("-i", "--zskip", type=int, dest="zskip", default=4, action='store', help="Skip this many z-slices between centers of max projections.")
+    args = parser.parse_args()
+    
+def mean_nfs_npz(fname):
+    """
+    Iterate through codestacks and average norm factors.
+    """
+    data = np.load(fname)
+    nfs = data['norm_factors'].tolist()
+    return np.mean([n for k, n in nfs.items()], axis=0)
+
+def load_codestack_from_npz(fname):
+    """
+    Load saved codestack data.
+    
+    Parameters
+    ----------
+    fname : str, filestream
+    
+    Returns
+    -------
+    cstk : dict
+        Dictionary of (y,x,nbits) arrays at different Z's
+    nfs : array
+        
+    class_imgs : dict
+        Dictionary of (y,x) arrays at different Z's of gene classifications
+    """
+    data = np.load(fname)
+    cstk = data['cstks'][()]
+    class_imgs = data['class_imgs'][()]
+    nfs = data['norm_factors'][()]
+    return cstk, nfs, class_imgs
+
+
+def classify_codestack(cstk, norm_vector, codeword_vectors, csphere_radius=0.5176):
+    """
+    Pixel based classification of codestack into gene_id pixels.
+    
+    Parameters
+    ----------
+    cstk : ndarray
+        Codestack (y,x,codebit)
+    norm_vector : array
+        Array (ncodebits,1) used to normalization intensity variation between codebits.
+    codeword_vectors : ndarray
+        Vector Normalized Array (ncodewords, ncodebits) specifying one bits for each gene
+    csphere_radius : float
+        Radius of ndim-sphere serving as classification bubble around codeword vectors
+    
+    Returns
+    -------
+    class_img : ndarray
+        Array (y,x) -1 if not a gene else mapped to index of codeword_vectors
+    """
+    cstk = cstk.copy()
+    cstk = cstk.astype('float32')
+    # Normalize for intensity difference between codebits
+    cstk = np.divide(cstk, norm_vector)
+    # Prevent possible underflow/divide_zero errors
+    np.place(cstk, cstk<=0, 0.01)
+    # Fill class img one column at a time
+    class_img = np.empty((cstk.shape[0], cstk.shape[1]))
+    for i in range(cstk.shape[0]):
+        v = cstk[i, :, :]
+        # l2 norm note codeword_vectors should be prenormalized
+        v = normalize(v, norm='l2')
+        # Distance from unit vector of codewords and candidate pixel codebits
+        d = distance_matrix(codeword_vectors, v)
+        # Check if distance to closest unit codevector is less than csphere thresh
+        dmin = np.argmin(d, axis=0)
+        dv = [i if d[i, idx]<csphere_radius else -1 for idx, i in enumerate(dmin)]
+        class_img[i, :] = dv
+    return class_img#.astype('int16')
+
+def mean_one_bits(cstk, class_img):#, nbits = 18):
+    """
+    Calculate average intensity of classified pixels per codebits.
+    
+    Parameters
+    ----------
+    cstk : ndarray
+        Codestack
+    class_img : ndarray
+        classification image
+    nbits : int
+        Number of codebits (maybe just make it equal to cstk shape?)
+    
+    Returns
+    -------
+    mean_bits : array
+        Mean of classified pixels for each codebit
+    """
+    bitvalues = defaultdict(list)
+    cstk = cstk.astype('float32')
+    nbits = cstk.shape[2]
+    for i in range(cvectors.shape[0]):
+        x, y = np.where(class_img==i)
+        if len(x) == 0:
+            continue
+        onebits = np.where(cvectors[i,:]==1)[0]
+        if len(onebits)<1:
+            continue
+        for i in onebits:
+            bitvalues[i].append(np.mean(cstk[x, y, i]))
+    mean_bits = np.array([robust_mean(bitvalues[i]) for i in range(nbits)])
+    return mean_bits
+
+def robust_mean(x):
+    return np.average(x, weights=np.ones_like(x) / len(x))
+
+def reclassify_file(f, nfactor):
+    """
+    Wrapper for iterative classification.
+    """
+    pth, pos = os.path.split(f)
+    print(pos)
+    a = pickle.load(open(os.path.join(base_path, p), 'rb'))
+    cstk = a['cstk']
+    #class_img = a['class_img']
+    new_class_img = classify_codestack(cstk, nfactor, nvectors)  
+    pickle.dump({'cstk': cstk, 'nf': nfactor,
+                 'class_img': new_class_img}, open(f, 'wb'))
+    return mean_one_bits(cstk, new_class_img)
+#     np.savez(os.path.join(base_path, p), cstk=cstk,
+#              class_img=new_class_img, norm_factor=nfactor)
+                    
+def classify_file(f, nfactor, nvectors):
+    """
+    Wrapper for classify_codestack. Can change this instead of function if 
+    intermediate file storage ever changes.
+    """
+    try:
+        pth, pos = os.path.split(f)
+        #print(pos)
+        cstks, nfs, class_imgs = load_codestack_from_npz(f)
+        for z in nfs.keys():
+            cstk = cstks[z]
+            new_class_img = classify_codestack(cstk, nfactor, nvectors)
+            class_imgs[z] = new_class_img
+            new_nf = mean_one_bits(cstk, new_class_img)
+            nfs[z] = new_nf
+        np.savez(os.path.join(pth, pos), cstks=cstks, norm_factors=nfs, class_imgs=class_imgs)
+    except:
+        return f
+#     pickle.dump({'cstk': cstk, 'nf': nfactor,
+#                  'class_img': new_class_img}, open(f, 'wb'))
+    #return np.mean([n for k, n in nfs.items()], axis=0)
+
+def unix_find(pathin):
+    """Return results similar to the Unix find command run without options
+    i.e. traverse a directory tree and return all the file paths
+    """
+    return [os.path.join(path, file)
+            for (path, dirs, files) in os.walk(pathin)
+            for file in files]
+    
+if __name__ == '__main__':
+    os.environ['MKL_NUM_THREADS'] = '4'
+    os.environ['GOTO_NUM_THREADS'] = '4'
+    os.environ['OMP_NUM_THREADS'] = '4'
+    print(args)
+    # Assuming these get imported during call below:
+    # 1. bitmap
+    # 2. bids, blanks, gids, cwords, gene_codeword_vectors, blank_codeword_vectors
+    # 3. norm_gene_codeword_vectors, norm_blank_codeword_vectors
+    
+    seqfish_config = importlib.import_module(args.cword_config)
+    bitmap = seqfish_config.bitmap
+    normalized_gene_vectors = seqfish_config.norm_gene_codeword_vectors
+    
+    codestacks = unix_find(args.cstk_path)
+    print(codestacks)
+    # iterative norm factor finding
+# Note preceding blocks and this can be noisy if restarted after crash etccc
+    with multiprocessing.Pool(args.ncpu) as ppool:
+        failed_positions = []
+        for i in range(args.niter):
+            if i == 0:
+                cur_nf = np.array(np.nanmean([mean_nfs_npz(c) for c in codestacks], axis=0))
+                print('90th Percentile Normalization factors:', cur_nf, sep='\n')
+            else:
+                new_nfs = np.array([mean_nfs_npz(c) for c in codestacks])
+                cur_nf = np.nanmean(new_nfs, axis=0)
+                print(cur_nf)
+            classify_pfunc = partial(classify_file, nfactor=cur_nf, nvectors=normalized_gene_vectors)
+            results = ppool.map(classify_pfunc, codestacks)
+            # Returns None if execution successful else returns fname
+            for idx, r in enumerate(results):
+                if r is None:
+                    continue
+                    print('None')
+                else:
+                    failed_positions.append(r)
+                    codestacks.remove(r)
+                    print(r)
+    print('These positions failed during classification: ', failed_positions)
+    
