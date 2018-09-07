f7e498998630221747e15318748fb7cb960ee6a8
rfor10 <rfor10@gmail.com>
2018-09-07 12:45:33 -0700

    slight improvements

--- analysis_scripts/find_beads.py
+++ analysis_scripts/find_beads.py
@@ -12,12 +12,15 @@ from skimage.feature import match_template, peak_local_max
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("analysis_path", type=str, help="Path to where you want your data saved.")
     parser.add_argument("md_path", type=str, help="Path to where your images are unless they are in Analysis/deconvolved/.",default = 'None')
+    parser.add_argument("analysis_path", type=str, help="Path to where you want your data saved.")
     parser.add_argument("-p", "--nthreads", type=int, dest="ncpu", default=6, action='store', help="Number of cores to utilize (default 6).")
+    parser.add_argument("-z", "--zindexes", type=int, nargs='*', action='store', dest="zindexes", default=-1)
+    parser.add_argument("-b", "--beadprofile", type=str, action='store', dest="ave_bead_path",
+                        default='/bigstore/Images2018/Zach/FISH_Troubleshooting/Transverse_Second_2018Aug24/Analysis/results/Avg_Bead.pkl')
     args = parser.parse_args()
 
-    
+
 def keep_which(stk, peaks, w=3, sz = 7):
     imsz = stk.shape
     keep_list = []
@@ -54,7 +57,7 @@ def find_beads_3D(fnames_list, bead_template, match_threshold=0.75):
     tforms : dict
         Dictionary of translation vectors (x, y, z)? maybe (y, x, z)
     """
-    hybe_names = ['hybe1', 'hybe2', 'hybe3', 'hybe4', 'hybe5', 'hybe6']
+    #hybe_names = ['hybe1', 'hybe2', 'hybe3', 'hybe4', 'hybe5', 'hybe6']
     ref_stk = numpy.stack([io.imread(i) for i in fnames_list], axis=2).astype('float')
     ref_match = match_template(ref_stk, bead_template)
     ref_beads = peak_local_max(ref_match, threshold_abs=match_threshold)
@@ -73,53 +76,62 @@ def add_bead_data(fnames_dict, pos, ave_bead):
                 beads = find_beads_3D(fnames_dict[h], ave_bead)
                 bead_dict[h] = beads
         print('Finished', pos)
-        pickle.dump(bead_dict, open(os.path.join(bead_path,pos), 'wb'))
+        
     else:
         bead_dict = {}
         for h in fnames_dict.keys():
             beads = find_beads_3D(fnames_dict[h], ave_bead)
             bead_dict[h] = beads
         print('Finished', pos)
-        pickle.dump(bead_dict, open(os.path.join(bead_path,pos), 'wb'))
+    pickle.dump(bead_dict, open(os.path.join(bead_path,pos), 'wb'))
     
 if __name__ == '__main__':
     #Setting up paths
-    if not os.path.exists(args.analysis_path):
-        os.makdirs(args.analysis_path)
-    if args.md_path == 'None':
-        deconvolved_path = os.path.join(args.analysis_path,'deconvolved')
+    analysis_path = args.analysis_path
+    md_path = args.md_path
+    zindexes = args.zindexes
+    ncpu = args.ncpu
+    ave_bead_path = args.ave_bead_path
+    if not os.path.exists(analysis_path):
+        os.makedirs(analysis_path)
+    if md_path == 'None':
+        deconvolved_path = os.path.join(analysis_path,'deconvolved')
         if not os.path.exists(deconvolved_path):
             print('analysis path doesnt have your metadata')
             print('add path to metadata after analysis path')
     else:
-        deconvolved_path = args.md_path
-    bead_path = os.path.join(args.analysis_path,'beads')
+        deconvolved_path = md_path
+    bead_path = os.path.join(analysis_path,'beads')
     if not os.path.exists(bead_path):
         os.makedirs(bead_path)
-    results_path = os.path.join(args.analysis_path,'results')
+    results_path = os.path.join(analysis_path,'results')
     if not os.path.exists(results_path):
         os.makedirs(results_path)
     
     #Retreiving file names and position names
     md = Metadata(deconvolved_path)
-    posnames = md.image_table.Position.unique()
-    posnames = [pos for pos in posnames if "Pos" in pos]
-    hybe_list = ['hybe1', 'hybe2', 'hybe3', 'hybe4', 'hybe5','hybe6', 'hybe7', 'hybe8', 'hybe9']
-    fnames_dicts = [md.stkread(Channel='DeepBlue', Position=pos,
+    md.image_table = md.image_table[[True if 'hybe' in i else False for i in md.image_table.acq]]
+    posnames = md.posnames
+    hybe_list = sorted([i.split('_')[0] for i in md.acqnames if 'hybe' in i])
+    if zindexes == -1:
+        fnames_dicts = [md.stkread(Channel='DeepBlue', Position=pos,
                            fnames_only=True, groupby='acq', 
                           hybe=hybe_list) for pos in posnames]
+    else:
+        fnames_dicts = [md.stkread(Channel='DeepBlue', Position=pos,
+                           fnames_only=True, groupby='acq', 
+                          hybe=hybe_list, Zindex=zindexes) for pos in posnames]
+
     #should move this to a better place probably just replace pyspots ave bead and import it
-    Ave_Bead = pickle.load(open('/bigstore/Images2018/Zach/FISH_Troubleshooting/Transverse_Second_2018Aug24/Analysis/results/Avg_Bead.pkl', 'rb'))
-    
+    Ave_Bead = pickle.load(open(ave_bead_path, 'rb'))
+
     #Setting up parrallel pool
     os.environ['MKL_NUM_THREADS'] = '3'
     os.environ['GOTO_NUM_THREADS'] = '3'
     os.environ['OMP_NUM_THREADS'] = '3'
-    ppool = mp.Pool(args.ncpu)
-    
     #Finding Beads
-    ppool.starmap(add_bead_data, zip(fnames_dicts, posnames, repeat(Ave_Bead, len(posnames))))
-    
+    with mp.Pool(ncpu) as ppool:
+        ppool.starmap(add_bead_data, zip(fnames_dicts, posnames, repeat(Ave_Bead, len(posnames))))
     #Combining beads to one file
     bead_dicts = defaultdict(dict)
     for files in os.listdir(bead_path):
