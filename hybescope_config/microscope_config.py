b3170c6aab6295ed0ef06563be76e6e9ab602d5f
rfor10 <rfor10@gmail.com>
2018-11-09 09:14:42 -0800

    updated chromatic abberation stuffs

--- hybescope_config/microscope_config.py
+++ hybescope_config/microscope_config.py
@@ -10,16 +10,29 @@ base_pth = os.path.dirname(__file__) # finds location of this module
 ave_bead = pickle.load(open(os.path.join(base_pth, 'ave_bead.333um.pkl'), 'rb'))
 hot_pixels = pickle.load(open(os.path.join(base_pth, 'hot_pixels_aug2018.pkl'), 'rb'))
 
-chromatic_dict = pickle.load(open(os.path.join(base_pth, 'jan2018_chromatic.pkl'), 'rb')) # Warning File import
+# Pre october 2018
+# chromatic_dict = pickle.load(open(os.path.join(base_pth, 'jan2018_chromatic.pkl'), 'rb')) # Warning File import
 
-xshift_fr = numpy.add(chromatic_dict['orange_minus_farred'][0], range(image_size[0]))
-yshift_fr = numpy.add(chromatic_dict['orange_minus_farred'][1], range(image_size[1]))
+# xshift_fr = numpy.add(chromatic_dict['orange_minus_farred'][0], range(image_size[0]))
+# yshift_fr = numpy.add(chromatic_dict['orange_minus_farred'][1], range(image_size[1]))
 
-xshift_g = numpy.add(chromatic_dict['orange_minus_green'][0], range(image_size[0]))
-yshift_g = numpy.add(chromatic_dict['orange_minus_green'][1], range(image_size[1]))
+# xshift_g = numpy.add(chromatic_dict['orange_minus_green'][0], range(image_size[0]))
+# yshift_g = numpy.add(chromatic_dict['orange_minus_green'][1], range(image_size[1]))
 
-xshift_db = numpy.add(chromatic_dict['orange_minus_deepblue'][0], range(image_size[0]))
-yshift_db = numpy.add(chromatic_dict['orange_minus_deepblue'][1], range(image_size[1]))
+# xshift_db = numpy.add(chromatic_dict['orange_minus_deepblue'][0], range(image_size[0]))
+# yshift_db = numpy.add(chromatic_dict['orange_minus_deepblue'][1], range(image_size[1]))
+# Post october 2018
+# Green - Other Channel
+chromatic_dict = pickle.load(open(os.path.join(base_pth, 'chromatic_october2018.pkl'), 'rb'))
+
+xshift_fr = numpy.add(chromatic_dict['FarRed']['x'].mean(axis=0), range(image_size[0]))
+yshift_fr = numpy.add(chromatic_dict['FarRed']['y'].mean(axis=1), range(image_size[1]))
+
+xshift_o = numpy.add(chromatic_dict['Orange']['x'].mean(axis=0), range(image_size[0]))
+yshift_o = numpy.add(chromatic_dict['Orange']['y'].mean(axis=1), range(image_size[1]))
+
+xshift_db = numpy.add(chromatic_dict['DeepBlue']['x'].mean(axis=0), range(image_size[0]))
+yshift_db = numpy.add(chromatic_dict['DeepBlue']['y'].mean(axis=1), range(image_size[1]))
 
 farred_psf = imread(os.path.join(base_pth, 'farred_psf_fit_250nmZ_63x.tif'))
 farred_psf = farred_psf[25, 8:17, 8:17]
