033c3cde83592c59c9c284df722a9dcb0391102f
zehemminger <zehemminger@g.ucla.edu>
2023-07-25 11:14:16 -0700

    Update merfish_config_TBI_Hippocampus.py

--- merfish_config_TBI_Hippocampus.py
+++ merfish_config_TBI_Hippocampus.py
@@ -53,10 +53,31 @@ bitmap = [('RS0095_cy5', 'hybe1', 'FarRed'),
             ('RSN1252.0_atto565', 'hybe17', 'FarRed'),
             ('RSN9535.0_atto565', 'hybe18', 'FarRed')]
 
+# FOr only 1 experiment 2 and 3 were flipped 2023jul10 (maybe 4 and 5 too)
+bitmap = [('RS0095_cy5', 'hybe1', 'FarRed'),
+            ('RS0109_cy5', 'hybe3', 'FarRed'),
+            ('RS0175_cy5', 'hybe2', 'FarRed'),
+            ('RS0237_cy5', 'hybe4', 'FarRed'),
+            ('RS0307_cy5', 'hybe5', 'FarRed'),
+            ('RS0332_cy5', 'hybe6', 'FarRed'),
+            ('RSN9927.0_cy5', 'hybe7', 'FarRed'),
+            ('RSN2336.0_cy5', 'hybe8', 'FarRed'),
+            ('RSN1807.0_cy5', 'hybe9', 'FarRed'),
+            ('RS0384_atto565', 'hybe10', 'FarRed'),
+            ('RS0406_atto565', 'hybe11', 'FarRed'),
+            ('RS0451_atto565', 'hybe12', 'FarRed'),
+            ('RS0468_atto565', 'hybe13', 'FarRed'),
+            ('RS0548_atto565', 'hybe14', 'FarRed'),
+            ('RS64.0_atto565', 'hybe15', 'FarRed'),
+            ('RSN4287.0_atto565', 'hybe16', 'FarRed'),
+            ('RSN1252.0_atto565', 'hybe17', 'FarRed'),
+            ('RSN9535.0_atto565', 'hybe18', 'FarRed')]
+
 nbits = len(bitmap)
 
 """ For Loading the Codebook"""
-codebook_pth = '/bigstore/binfo/Codebooks/TBI_Hippocampus_Codebook.txt'
+# codebook_pth = '/bigstore/binfo/Codebooks/TBI_Hippocampus_Codebook.txt'
+codebook_pth = '/greendata/binfo/Codebooks/TBI_Hippocampus_Codebook.txt'
 def load_codebook(fname):
     barcodes = []
     with open(fname, 'r') as f:
@@ -106,11 +127,11 @@ norm_all_codeword_vectors = normalize(all_codeword_vectors)
 """ MERFISH Code Parameters"""
 parameters = {}
 """ General """
-parameters['daemon_path']= '/bigstore/GeneralStorage/daemon' #'/scratch/daemon/' # Where should the Daemons Look
-parameters['utilities_path']= '/bigstore/GeneralStorage/utilities'#'/scratch/utilities/' # Where to save temporary files
-parameters['fishdata']='fishdata' #Directory Name for Processed Data >Bigstore>Images[Year]>User.Project>Dataset>fishdata
+parameters['daemon_path']= '/greendata/GeneralStorage/daemon' #'/scratch/daemon/' # Where should the Daemons Look
+parameters['utilities_path']= '/greendata/GeneralStorage/utilities'#'/scratch/utilities/' # Where to save temporary files
+parameters['fishdata']='fishdata_2D' #Directory Name for Processed Data >Bigstore>Images[Year]>User.Project>Dataset>fishdata
 parameters['verbose']=False # If you want print statements (Mostly for diagnostics)
-parameters['two_dimensional']=False #Work in 2 or 3 dimensions
+parameters['two_dimensional']=True #Work in 2 or 3 dimensions
 parameters['pixel_size'] = 0.083 #1.5 => 0.083 #1=> 0.123# size of pixel in um
 parameters['z_step_size'] = 0.4 # size of step in Z um
 """ Dataset """
@@ -128,11 +149,11 @@ parameters['dbscan_eps']=3
 parameters['dbscan_min_samples']=20 # How many beads you need
 parameters['max_dist']=int(15/parameters['pixel_size'])#15 um # Max distance a bead can be to be paired
 parameters['match_threshold']=0.65 # Peak Calling threshold for beads
-parameters['ref_hybe']='hybe1' # Name of Reference Hybe
+parameters['ref_hybe']='hybe10' # Name of Reference Hybe
 parameters['registration_channel']='DeepBlue' # What channel has your fiduciary markers
 parameters['registration_method'] = 'beads' # What method of registration to use (bead or image)
-parameters['registration_image_blur_kernel'] = 2 # What size kernel to blur image with
-parameters['registration_image_background_kernel'] = 10 # What size kernel to calculte background
+parameters['registration_image_blur_kernel'] = (0.1/parameters['pixel_size']) #100 nm # What size kernel to blur image with
+parameters['registration_image_background_kernel'] = (0.5/parameters['pixel_size']) # 500nm # What size kernel to calculte background
 parameters['subpixel_method'] = 'max'
 parameters['registration_overwrite'] = False
 """ Stack """
@@ -142,7 +163,7 @@ parameters['projection_zstart']=-1 # Which Z index to start (-1 means first)
 parameters['projection_k']=1 # How many Z above and below to include (1 means 1 above and 1 below)
 parameters['projection_zskip']=2 # How many Z indexes to skip
 parameters['projection_zend']=-1 # Which Z index to stop (-1 means last)
-parameters['projection_function']='mean' # Which method to use to project (typically max or mean)
+parameters['projection_function']='max' # Which method to use to project (typically max or mean)
 parameters['dtype_rel_min']=0 # when converting dtypes this amount in percentile will be set to 0
 parameters['dtype_rel_max']=100 # when converting dtypes this amount in percentile will be set to max
 parameters['dtype']='uint16' # dtype to save in
@@ -158,10 +179,10 @@ parameters['spot_diameter'] = 5 # 250 nm
 parameters['spot_minmass'] = 15#9 # not based on size?
 parameters['spot_separation'] = 3 # 100 nm
 parameters['image_call_spots'] = False
-parameters['image_overwrite'] = True
+parameters['image_overwrite'] = False
 """ Segment """
-parameters['nucstain_channel'] = 'DeepBlue' # Which Channel is your nuclear stain in
-parameters['nucstain_acq'] = 'nucstain' # Which acquision is your nuclear signal in
+parameters['nucstain_channel'] = 'FarRed' # Which Channel is your nuclear stain in
+parameters['nucstain_acq'] = 'hybe25' # Which acquision is your nuclear signal in
 parameters['segment_projection_function'] = 'mean' # method for projecting in Z
 parameters['segment_min_size'] = (100/parameters['pixel_size'])#100 um2 # min cell size in pixels to keep for 2D
 parameters['segment_overlap_threshold'] = 0.3 # % of overlap to merge cells in Z
@@ -169,7 +190,7 @@ parameters['segment_pixel_thresh'] = (100/parameters['pixel_size'])#100 um2 # mi
 parameters['segment_z_thresh'] = 0#5 # How many Z's a cell has to be in to keep
 parameters['segment_distance_thresh'] = 10 # distance to dialate cell in um
 parameters['segment_model_type']="nuclei" # cellpose model type
-parameters['segment_gpu'] = True # use gpu?
+parameters['segment_gpu'] = False # use gpu?
 parameters['segment_batch_size'] = 8 # how many batches to break up calculation
 parameters['segment_diameter'] = (10/parameters['pixel_size']) # size of cell in um2
 parameters['segment_channels'] = [0,0] # grey scale for cellpose
@@ -177,47 +198,57 @@ parameters['segment_flow_threshold'] = 1 # cellpose parameters
 parameters['segment_cellprob_threshold'] = 0 # cellpose parameters
 parameters['segment_downsample'] = 0.25 # amount to downsample images
 parameters['segment_two_dimensional'] = True#False # perform in 2D or 3D
-parameters['segment_overwrite'] = False # Overwrite previous segmentation?
+parameters['segment_overwrite'] = True # Overwrite previous segmentation?
 parameters['segment_nuclear_blur'] = (25/parameters['pixel_size']) # 25 um2 sigma in pixels for background
 parameters['segment_z_step_size'] = 0.4
 parameters['segment_pixel_size'] = parameters['pixel_size']
-parameters['segment_overwrite'] = True
 parameters['segment_ncpu'] = 30
 """ Classify """
+parameters['classification_method'] = 'spot'
 parameters['match_thresh'] = -2 # how many mismatched bits to be called a barcode
 parameters['fpr_thresh'] = 0.4 # euclidean distance from barcodes to be called
 parameters['classification_overwrite'] = True
+parameters['spot_percentile'] = 98
+parameters['spot_max_distance'] = 0.1
+parameters['spot_diameter'] = 5
+parameters['spot_minmass'] = 4
+parameters['spot_separation'] = 3
+parameters['classify_iterations'] = 1
 #parameters['logistic_columns'] = ['mass', 'size', 'ecc','intensity' 'raw_mass', 'ep', 'cword_distance', 'correct_bits', 'false_positives', 'false_negatives', 'signal', 'noise', 'signal-noise', 'X']
 parameters['logistic_columns'] = ['raw_mass', 'ep','intensity', 'signal', 'noise', 'signal-noise','X']
 spot_parameters = {}
 spot_parameters['default'] = {'spot_max_distance':3,
-                                       'spot_minmass':15,
+                                       'spot_minmass':5,
                                        'spot_diameter':5,
                                           'spot_separation':3}
-spot_parameters['sham1_3_2022Jan03'] = {'spot_max_distance':3,
-                                       'spot_minmass':12,
+spot_parameters['WT_2022Sep16'] = {'spot_max_distance':0.1,
+                                       'spot_minmass':4,
                                        'spot_diameter':5,
                                           'spot_separation':3}
-spot_parameters['Sham_2_4_2022Jan21'] = {'spot_max_distance':3,
-                                       'spot_minmass':17,
-                                       'spot_diameter':5,
-                                          'spot_separation':3}
-spot_parameters['Sham_3_4_2022Jan25'] = {'spot_max_distance':3,
-                                       'spot_minmass':15,
-                                       'spot_diameter':5,
-                                          'spot_separation':3}
-spot_parameters['TBI-1-1_2021Oct20'] = {'spot_max_distance':3,
-                                       'spot_minmass':15,
-                                       'spot_diameter':5,
-                                          'spot_separation':3}
-spot_parameters['TBI_3_1_2022Jan15'] = {'spot_max_distance':3,
-                                       'spot_minmass':19,
-                                       'spot_diameter':5,
-                                          'spot_separation':3}
-spot_parameters['TBI4_1_2022Jan11'] = {'spot_max_distance':3,
-                                       'spot_minmass':15,
-                                       'spot_diameter':5,
-                                          'spot_separation':3}
+# spot_parameters['sham1_3_2022Jan03'] = {'spot_max_distance':3,
+#                                        'spot_minmass':12,
+#                                        'spot_diameter':5,
+#                                           'spot_separation':3}
+# spot_parameters['Sham_2_4_2022Jan21'] = {'spot_max_distance':3,
+#                                        'spot_minmass':17,
+#                                        'spot_diameter':5,
+#                                           'spot_separation':3}
+# spot_parameters['Sham_3_4_2022Jan25'] = {'spot_max_distance':3,
+#                                        'spot_minmass':15,
+#                                        'spot_diameter':5,
+#                                           'spot_separation':3}
+# spot_parameters['TBI-1-1_2021Oct20'] = {'spot_max_distance':3,
+#                                        'spot_minmass':15,
+#                                        'spot_diameter':5,
+#                                           'spot_separation':3}
+# spot_parameters['TBI_3_1_2022Jan15'] = {'spot_max_distance':3,
+#                                        'spot_minmass':19,
+#                                        'spot_diameter':5,
+#                                           'spot_separation':3}
+# spot_parameters['TBI4_1_2022Jan11'] = {'spot_max_distance':3,
+#                                        'spot_minmass':15,
+#                                        'spot_diameter':5,
+#                                           'spot_separation':3}
 
 
 camera_direction_dict = {'default':[-1,-1],
